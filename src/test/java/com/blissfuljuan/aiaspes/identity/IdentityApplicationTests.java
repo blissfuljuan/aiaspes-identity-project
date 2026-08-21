aed4fd708a1095c000bd87ec041dedbe8812c3fe
Blissfuljuan <cirevil.1989@gmail.com>
2026-08-21 17:17:47 +0800

    Initial commit

---
+++ src/test/java/com/blissfuljuan/aiaspes/identity/IdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissfuljuan.aiaspes.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
